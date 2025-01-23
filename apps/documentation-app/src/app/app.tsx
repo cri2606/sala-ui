6628ec84c0c4ba2ea45520d2f3b37906bb5a464a
cristian <cristiansala894@gmail.com>
2025-01-23 16:01:54 +0100

    update form

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -3,11 +3,12 @@ import { Footer } from '@sala-ui/footer';
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
+import { Form } from '@sala-ui/form';
 
 export function App() {
   return (
     <div>
-      <Footer />
+      <Form />
     </div>
   );
 }
