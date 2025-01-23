dc6f7d715ef8f831debe220601e0dabceb73d22d
cristian <cristiansala894@gmail.com>
2025-01-23 16:44:11 +0100

    update grid

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Grid } from '@sala-ui/grid';
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
-import { Header } from '@sala-ui/header';
 
 export function App() {
   return (
     <div>
-      <Header />
+      <Grid />
     </div>
   );
 }
--- packages/grid/src/lib/grid.tsx
+++ packages/grid/src/lib/grid.tsx
@@ -2,9 +2,52 @@ import styles from './grid.module.css';
 
 export function Grid() {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Grid!</h1>
+    <>
+    {/* one column */}
+    <div className="flex mb-4">
+      <div className="w-full bg-gray-500 h-12"></div>
     </div>
+    
+    {/* two columns */}
+    <div className="flex mb-4">
+      <div className="w-1/2 bg-gray-400 h-12"></div>
+      <div className="w-1/2 bg-gray-500 h-12"></div>
+    </div>
+    
+    {/* three columns */}
+    <div className="flex mb-4">
+      <div className="w-1/3 bg-gray-400 h-12"></div>
+      <div className="w-1/3 bg-gray-500 h-12"></div>
+      <div className="w-1/3 bg-gray-400 h-12"></div>
+    </div>
+    
+    {/* four columns */}
+    <div className="flex mb-4">
+      <div className="w-1/4 bg-gray-500 h-12"></div>
+      <div className="w-1/4 bg-gray-400 h-12"></div>
+      <div className="w-1/4 bg-gray-500 h-12"></div>
+      <div className="w-1/4 bg-gray-400 h-12"></div>
+    </div>
+    
+    {/* five columns */}
+    <div className="flex mb-4">
+      <div className="w-1/5 bg-gray-500 h-12"></div>
+      <div className="w-1/5 bg-gray-400 h-12"></div>
+      <div className="w-1/5 bg-gray-500 h-12"></div>
+      <div className="w-1/5 bg-gray-400 h-12"></div>
+      <div className="w-1/5 bg-gray-500 h-12"></div>
+    </div>
+    
+    {/* six columns */}
+    <div className="flex">
+      <div className="w-1/6 bg-gray-400 h-12"></div>
+      <div className="w-1/6 bg-gray-500 h-12"></div>
+      <div className="w-1/6 bg-gray-400 h-12"></div>
+      <div className="w-1/6 bg-gray-500 h-12"></div>
+      <div className="w-1/6 bg-gray-400 h-12"></div>
+      <div className="w-1/6 bg-gray-500 h-12"></div>
+    </div>
+    </>
   );
 }
 
