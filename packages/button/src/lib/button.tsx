4f829d7f873dfd6628295bbb90bc9720c881feff
cristian <cristiansala894@gmail.com>
2025-01-23 14:51:22 +0100

    update button

--- packages/button/src/lib/button.tsx
+++ packages/button/src/lib/button.tsx
@@ -2,9 +2,7 @@ import styles from './button.module.css';
 
 export function Button() {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
-    </div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
   );
 }
 
