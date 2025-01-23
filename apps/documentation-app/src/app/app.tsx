f3b2b137e1d401dad170abc564c6eeb26862a072
cristian <cristiansala894@gmail.com>
2025-01-23 16:20:49 +0100

    update header

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -1,14 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Footer } from '@sala-ui/footer';
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
-import { Form } from '@sala-ui/form';
+import { Header } from '@sala-ui/header';
 
 export function App() {
   return (
     <div>
-      <Form />
+      <Header />
     </div>
   );
 }
